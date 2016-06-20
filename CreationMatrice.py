784c9d181d6168d3d030ad1f662447960a2dd193
Adrien Lagasse <adrien.lagasse@telecom-paristech.fr>
2016-06-20 11:38:20 +0200

    Adding matrix creation function

--- CreationMatrice.py
+++ CreationMatrice.py
@@ -38,27 +38,76 @@ def dic_mot_doc_occurence(list_dic_freq, list_titles) :
 
 
 def compute_tfidf2(dic_mot_doc_occurence, word, title, list_titles) :
-    tf = dic_mot_doc_occurence[word][title]
+    if title in dic_mot_doc_occurence[word] :
+        tf = dic_mot_doc_occurence[word][title]
+    else :
+        return 0
     idf= len(list_titles)/len(dic_mot_doc_occurence[word])
     return tf*np.log(idf)
     
 def create_matrix(dic_mot_doc_occurence, list_titles) :
-    list_not_null_values =[]
-    list_
+    list_not_null_values = []
+    list_columns = []
+    list_lines = []
+    list_words = []
+    i = 0
+    tfidf = 0
+    for word in dic_mot_doc_occurence :
+        for j in range(len(list_titles)) :
+            title = list_titles[j]
+            tfidf = compute_tfidf2(dic_mot_doc_occurence, word, title, list_titles)
+            if tfidf != 0 :
+                list_not_null_values.append(tfidf)
+                list_columns.append(i)
+                list_words.append(word)
+                list_lines.append(j)
+        i=i+1
+    return [list_not_null_values, list_columns, list_words, list_lines]
     
+def create_matrix2(dic_mot_doc_occurence, list_titles) :
+    list_not_null_values = []
+    dic_columns = dict()
+    dic_lines = dict()
+    i = 0
+    tfidf = 0
+    for word in dic_mot_doc_occurence :
+        for j in range(len(list_titles)) :
+            title = list_titles[j]
+            tfidf = compute_tfidf2(dic_mot_doc_occurence, word, title, list_titles)
+            if tfidf != 0 :
+                list_not_null_values.append(tfidf)
+                dic_columns[i] = word
+                dic_lines[j] = title
+        i=i+1
+    return [list_not_null_values, dic_columns, dic_lines]
 
 
+
+
+def compute_tfidf3(dic_mot_doc_occurence, word, title, list_titles) :
+    if title in dic_mot_doc_occurence[word] :
+        tf = dic_mot_doc_occurence[word][title]
+    idf= len(list_titles)/len(dic_mot_doc_occurence[word])
+    return tf*np.log(idf)
 
 
 
 dic_freq1 = dict()
 dic_freq2 = dict()
+dic_freq3 = dict()
+
 dic_freq1["poisson"] = 0.5
 dic_freq1["a"]=0.4
 dic_freq1["b"]=0.6
-dic_freq2["viande"] = 0.5
+
+dic_freq2["viande"] = 0.6
 dic_freq2["a"]=0.7
 dic_freq2["b"]=0.8
-list_dic_freq = [dic_freq1, dic_freq2] 
-list_titles= ["ninja", "samourai"]
+
+dic_freq3["oeuf"] = 0.4
+dic_freq3["a"]=0.7
+dic_freq3["c"]=0.2
+
+list_dic_freq = [dic_freq1, dic_freq2, dic_freq3] 
+list_titles= ["ninja", "samourai", "wasabi"]
 dic = dic_mot_doc_occurence(list_dic_freq, list_titles)
