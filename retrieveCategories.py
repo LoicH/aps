23c14050c4cf383d2c9316d833bb321d471378d4
Antoine SUEUR <antoine.sueur@free.fr>
2016-06-20 16:55:46 +0200

    added method to get URIs of words in text and linked categories

--- retrieveCategories.py
+++ retrieveCategories.py
@@ -6,13 +6,36 @@ Created on Mon Jun 20 16:24:43 2016
 """
 
 import spotlight
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 
-
-def getURIs(text):
+def getURIs(text): 
     L=[]
     annotations = spotlight.annotate('http://spotlight.sztaki.hu:2222/rest/annotate',text,confidence=0.4, support=20, spotter='Default')
     for i in annotations:
-        L.append[i["URI"]]
-        print i["URI"]
-                           
+        a=i["URI"]
+        L.append(a.encode("UTF-8"))
+    return L
+#problem to solve : one URI missing when duplicated
+                          
+
+
+    
+def getCategories(URIList):
+    L=[]
+    for URI in URIList:
+        URI="<http://dbpedia.org/page/Computer_science>"
+        sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+        sparql.setQuery("""
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            SELECT ?label
+            WHERE { """+ URI + " dc:subject ?label }"
+        )
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        for result in results["results"]["bindings"]:
+            print(result["label"]["value"])
+            L.append(result["label"]["value"])
+    return L
+    
+#to test
