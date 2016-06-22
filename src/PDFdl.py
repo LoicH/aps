80fc14fb8d678a100ba21c10e9cd377a107e3d8b
Antoine SUEUR <antoine.sueur@free.fr>
2016-06-22 17:06:43 +0200

    added script to download all PDF from a bibTex file

--- src/PDFdl.py
+++ src/PDFdl.py
@@ -7,73 +7,46 @@ Created on Tue Jun 21 18:08:59 2016
 
 import urllib
 import re
+import bibtexparser
+import os
 
-# regex=r'id=(\d+)}'  
+#constantes
+app_path = os.getcwd().split(os.sep+"aps")[0]+os.sep+"aps"
+data = app_path+os.sep+"data"
+src = app_path+os.sep+"src"
+regex=r'id=(\d+)'  
 
+def openBibLib(bibName): # e.g : 'document.bib'
+    with open('document.bib') as bibtex_file:  
+        bibtex_database = bibtexparser.load(bibtex_file) 
+        return bibtex_database
 
-
-class PDF(object):
-    """__init__() functions as the class constructor"""
-    def __init__(self, title=None, author=None, id=None, pubDate=None, status=None):
-        self.title = title
-        self.author = author
-        self.id = id
-        self.pubDate = pubDate
-        
 
 def download(fileURL,writeFile):
     testfile = urllib.URLopener()
-    testfile.retrieve(fileURL, writeFile)  # example : testfile.retrieve("http://biblio.telecom-paristech.fr/cgi-bin/download.cgi?id=11068", "file.pdf")
-
-
-
-
-file = open("document.bib", "r")
-lines = file.readlines()
-str = ''                   #str = text of the file document.bib
-for i in range(len(lines)):
-    str += lines[i].rstrip('\n') + ' '
-
-
-pdfs=re.split(r'@[article|inproceedings|workshop|invite|incollection|grandpublic|proceedings|multimedia|book|inbook|booklet|manual|phdthesis|mastersthesis|rapcontrat|raprech|raphdr|techreport|reference|standardisation|brevet|unpublished|misc]+',str)
-
-def createPDF(index):
-    c=pdfs[index]
-    fields=c.split(",") #fields separation
-    for i in range(len(fields)):    
-        fields[i]=fields[i].split("=")  #values separation
-    i=0
-    regex2=r'(\d+)'
-    pubDate=''
-    while i<len(fields):
-        if "author" in fields[i][0]:
-            author=fields[i][1]
-        if "title" in fields[i][0] and not "booktitle" in fields[i][0]:
-            title=fields[i][1]
-        if "year" in fields[i][0]:
-            year=fields[i][1]
-            pubDate+=year
-        if "month" in fields[i][0]:
-            month=fields[i][1]
-            pubDate+=month
-        if(index>3110):
-            print fields[i]
-        if "annote" in fields[i][0]:
-            temp =re.findall(regex2,fields[i][-1])
-            if len(temp)==0:
-                return 
-            else:
-                id=int(temp[0])
-        i+=1
-    return PDF(title,author,id,pubDate)
-    
-    
-def createPDFList():
-    L=[]
-    for i in range(1,len(pdfs)):
+    try:
+        testfile.retrieve(fileURL, writeFile)  # example : testfile.retrieve("http://biblio.telecom-paristech.fr/cgi-bin/download.cgi?id=11068", "file.pdf")
+    except IOError:
+        print "No pdf"
+        
+def downloadPDF(id):
+    testfile = urllib.URLopener()
+    print  "http://biblio.telecom-paristech.fr/cgi-bin/download.cgi?id="+str(id)
+    try:
+        if( str(id)+".pdf" in os.listdir(data)): #checks if the file already exists
+            print "File already present"
+        else:
+            testfile.retrieve("http://biblio.telecom-paristech.fr/cgi-bin/download.cgi?id="+str(id), data+os.sep+str(id)+".pdf")
+    except IOError:
+        print "No pdf"
+def downloadAll(bibName):
+    bibtex_database=openBibLib(bibName)
+    for i in range(len(bibtex_database.entries)):
+        try:
+            annote=bibtex_database.entries[i]["annote"]
+        except KeyError:
+            print "No id found"
         print i
-        pdf=createPDF(i)
-        if pdf is not None:
-            L.append(pdf)
-            print(pdf.title)
-    return L
+        id=int(re.findall(regex,annote)[0])
+        downloadPDF(id)
+
