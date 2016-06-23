6a38d4230263923c4f9039444f4a56a5383d9aec
LoicH <loic.herbelot@free.fr>
2016-06-23 12:25:33 +0200

    Moving js files to static/

--- src/server.py
+++ src/server.py
@@ -8,7 +8,6 @@ import os
 app_path = os.getcwd().split(os.sep+"aps")[0]+os.sep+"aps"
 data = app_path+os.sep+"data"
 src = app_path+os.sep+"src"
-js = app_path+os.sep+"js"
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -17,10 +16,18 @@ app = Flask(__name__)
 def index():
     return "Hello world!"
     
-@app.route('/wordcloud')
+@app.route('/wordcloud/')
 def wordcloud():
+    jsonData = src+os.sep+"templates"+os.sep+"WordCloud.js"
+    d3wordcloudjs = src+os.sep+"templates"+os.sep+"d3.layout.cloud.js"
     return render_template('WordCloud.html')    
     #return '<script type="text/javascript" src="'+data+os.sep+'WordCloud.js"></script>'
+    
+@app.route('/author/<name>')
+def show_author(name):
+    return "Welcome to the overview of <b> %s" % name
 
 if __name__ == "__main__":
+    print "Server running in",os.getcwd()
     app.run(debug=True, host='0.0.0.0')
+    
