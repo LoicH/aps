6364e072c8173033a0b3118258e472e22b6c2834
Antoine SUEUR <antoine.sueur@free.fr>
2016-06-24 11:47:07 +0200

    began a script to create TFIDF matrixes for every document in a given time

---
+++ src/dataTimeline.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 24 10:43:42 2016
+
+@author: asueur
+"""
+from freqMatrixClass import FreqMatrix
+from retrieveCategories import getAll
+from timelineVisualization import getInfo
+#tfid concepts : matrice
+# sommer par lignes et normaliser /total
+#faire moyennne pour 3 annnees
+#convertir au bon format
+
+
+
+def createTFIDMatrix(authorName, startDate, endDate): #date format : year monthNumber
+    a = dict() #dictionary of frequency dictionaries for documents
+    listTitle=getInfo(authorName)[0] #dictonnary of publications and dates and ids
+    titleId=dict()
+    for j in listTitle:
+        if formatDate(j.values[0])>startDate and formatDate(j.values[0])<endDate:
+            titleId[j]=j.values[1]
+    for title in titleId: #a modifier pour que ce soit la liste des textes et pas des titres
+        fileName=str(titleId[title])+".txt"
+        fileText = fileName.read()
+        a[title]=(getAll(fileText)[0]) #category frequencies of documents
+    fm=FreqMatrix([],[])
+    for k in a:
+        fm.add_doc(k,a[k])
+    return fm.to_TFIDF_Matrix()
--- src/timelineVisualization.py
+++ src/timelineVisualization.py
@@ -7,8 +7,9 @@ Created on Thu Jun 23 11:19:24 2016
 
 import PDFdl
 import bibtexparser
+import re
 
-
+regex=r'id=(\d+)' 
 bib = 'document.bib'  ### bib name (can be modified)
 entries=PDFdl.openBibLib(bib).entries #all of the bib's articles
 
@@ -21,14 +22,14 @@ class article:
 def getAll(index):
     return bibtexparser.customization.convert_to_unicode(entries[index]) 
 
-def getInfo(authorName): #name in the form of Initial. lastName
-    dates = dict()
+def getInfo(authorName): #name in the form of lastName
+    datesAndIds = dict()
     coauthors=[]
     temp=0
     for i in entries:
         if authorName in i["author"]:
             try:
-                dates[i["title"]]=i["year"]+" " + i["month"]
+                datesAndIds[i["title"]]=[i["year"]+" " + i["month"],int(re.findall(regex,i["annote"])[0])] #dictionnary pubName : [date,id]
             except KeyError:
                 print "no month available for document"
                 temp+=1
@@ -39,7 +40,7 @@ def getInfo(authorName): #name in the form of Initial. lastName
     
     coauthorsFreq = {a:coauthors.count(a) for a in coauthors}    
     
-    return dates, coauthorsFreq
+    return datesAndIds, coauthorsFreq
         
 
     
