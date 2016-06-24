b27a0cf354f0ff11df60c4c53b29d30bd83c1b49
LoicH <loic.herbelot@free.fr>
2016-06-24 17:31:02 +0200

    Modifying modifTexte.py to remove also one letter words

--- src/main.py
+++ src/main.py
@@ -22,16 +22,26 @@ src = app_path+os.sep+"src"
 
 ##Getting the text from all PDF 
 print "Getting the text from the PDF"
-for filename in os.listdir(data):
+
+#get all the PDF files
+files = os.listdir(data)
+pdfs = []
+for filename in files:
     matchObj = re.match(r'(.*)\.pdf', filename)
     if matchObj:
-        try:
-            docname = matchObj.group(1)
-            if docname+"_out.txt" not in os.listdir(data):
-                print "Retrieving text of",docname
-                pdf2txt.pdf_to_file(data+os.sep+filename, data+os.sep+docname+"_out.txt")
-        except:
-            print "Problem getting the name of the file \"",filename,"\"."
+        pdfs.append(matchObj.group(1))
+
+count = 0
+total = len(pdfs)
+for docname in pdfs:
+    count += 1
+    print "(",count,"/",total,")"
+    try:
+        if docname+"_out.txt" not in os.listdir(data):
+            print "Retrieving text of",docname
+            pdf2txt.pdf_to_file(data+os.sep+filename, data+os.sep+docname+"_out.txt")
+    except:
+        print "Problem opening \"",filename,"\"."
 
 
 #Constructing frequency dictionary for all documents
@@ -39,17 +49,31 @@ freqMatrix = FreqMatrix([],[])
 
     
 print "Analyzing text of PDF"
-
-for filename in os.listdir(data):
-    if re.match(r'(.*)_out\.txt', filename):
-        print filename
-        f = open(data+os.sep+filename, 'r')
-        txt = f.read()
-        f.close()
+out_txts = []
+for filename in files:
+    matchObj = re.match(r'(.*)_out\.txt', filename)
+    if matchObj:
+        out_txts.append(filename)
+count = 0
+total = len(out_txts)
+not_english = []
+for filename in out_txts:
+    count += 1
+    print "(",count,"/",total,")"
+    print filename
+    f = open(data+os.sep+filename, 'r')
+    txt = f.read()
+    f.close()
+    if modifTexte.estimate_language(txt) == 'english':
         dic = modifTexte.textToDictionnary(txt.decode("utf-8"),[])
         l = sorted([(v,k) for (k,v) in dic.items()], reverse=True)[:99]
         first = {k:v for (v,k) in l}
         freqMatrix.add_doc(filename[:-8], first)
+    else:
+        print filename,"is not in english!"
+        not_english.append(filename)
+
+print "Docs not in english:",not_english
 
 #frequency matrix is done at this point
 freqMatrix.save(data+os.sep+"freq"+time.strftime("%d-%m-%y-%Hh%M")+".csv")
--- src/modifTexte.py
+++ src/modifTexte.py
@@ -11,61 +11,55 @@ Created on Thu Jun 16 16:42:50 2016
 
 # blackList : banned words list
 import string
-from nltk import *
+import nltk
 from nltk.corpus import wordnet
-
-def get_wordnet_pos(treebank_tag):
-
-    if treebank_tag.startswith('J'):
-        return wordnet.ADJ
-    elif treebank_tag.startswith('V'):
-        return wordnet.VERB
-    elif treebank_tag.startswith('N'):
-        return wordnet.NOUN
-    elif treebank_tag.startswith('R'):
-        return wordnet.ADV
-    else:
-        return ''
 
 
 
 #tests
-pdfPath = "/cal/homes/asueur/Downloads/TP3.pdf"
-text1 = "abc Abc bAc.... bBc bacb acb acb acb and ant ham"
-randomText="""Still court no small think death so an wrote. Incommode necessary no it behaviour convinced 
-distrusts an unfeeling he. Could death since do we hoped is in. Exquisite no my attention extensive. 
-The determine conveying moonlight age. Avoid for see marry sorry child. Sitting so totally 
-forbade hundred to.Their could can widen ten she any. As so we smart those money in. Am wrote up whole 
-so tears sense oh. Absolute required of reserved in offering no. How sense found our those gay again 
-taken the. Had mrs outweigh desirous sex overcame. Improved property reserved disposal do offering me"""
-blackList=["abc","acb"]
+#if __name__ == "__main__":
+    
+    #pdfPath = "/cal/homes/asueur/Downloads/TP3.pdf"
+    #text1 = "abc Abc bAc.... bBc bacb acb acb acb and ant ham"
+    #randomText="""Still court no small think death so an wrote. Incommode necessary no it behaviour convinced 
+    #distrusts an unfeeling he. Could death since do we hoped is in. Exquisite no my attention extensive. 
+    #The determine conveying moonlight age. Avoid for see marry sorry child. Sitting so totally 
+    #forbade hundred to.Their could can widen ten she any. As so we smart those money in. Am wrote up whole 
+    #so tears sense oh. Absolute required of reserved in offering no. How sense found our those gay again 
+    #taken the. Had mrs outweigh desirous sex overcame. Improved property reserved disposal do offering me"""
+    #blackList=["abc","acb"]
 
 
 #deleting punctuation
 def delPunctuation(text):
     for char in string.punctuation: 
         text=text.replace(char,'')
-    return text
+    return text.replace('\x0c','')
     
 def getTextWords(text):
-    return word_tokenize(text)
+    return nltk.word_tokenize(text)
 
 #deleting useless words + lowercase
 def modifyText(textWords, blackList):
-    for i in range(len(textWords)):
-        if not textWords[i].isupper():
-            textWords[i]=textWords[i].lower()
-    for i in blackList :
-        textWords = textWords.replace(i,'')
+    i=0
+    for word in textWords:
+        if len(word) == 1:
+            textWords.remove(word)
+            i -= 1
+        elif not word.isupper():
+            textWords[i]=word.lower()
+        i += 1
+    for w in blackList :
+        textWords = textWords.replace(w,'')
     return textWords
     
 def deleteStopWords(textWords):
     #deleting words of types determiners, conjonctions, cardinal numbers and stopwords
     listCopy=textWords
-    stopwords= corpus.stopwords.words("english")
+    stopwords= nltk.corpus.stopwords.words("english")
     for sw in stopwords:
         listCopy=[j for j in listCopy if j!=str(sw)]
-    types = pos_tag(listCopy)    
+    types = nltk.pos_tag(listCopy)    
     #print types
     for i in types:
         if i[1] in ["DT","CC", "CD", "PRP", "PRP$", "PDT"] :  
@@ -110,15 +104,15 @@ def get_wordnet_pos(treebank_tag): # convert pos_tag from nltk to pos_tag from w
 def lemmatization(textWords): 
     #lemmatizes all the words
     words = textWords
-    p = pos_tag(text)
-    lmtzr=WordNetLemmatizer()
+    p = nltk.pos_tag(textWords)
+    lmtzr=nltk.WordNetLemmatizer()
     for i in range(len(p)):
         words[i]=lmtzr.lemmatize(p[i][0], get_wordnet_pos(p[i][1]))
     return words
         
 def frequency(textWords):
     #gives the frequency of each word in the text
-    a=FreqDist(textWords)
+    a=nltk.FreqDist(textWords)
     nbMots = 0
     for word in a :
         nbMots = nbMots + a[word]
@@ -127,7 +121,7 @@ def frequency(textWords):
     return a
 
 def frequencyWithTreshold(textWords, occTreshold, lengthTreshold):
-    a=FreqDist(textWords)
+    a=nltk.FreqDist(textWords)
     nbMots = 0
     wordsToRemove = []
     for word in a :
@@ -141,8 +135,8 @@ def frequencyWithTreshold(textWords, occTreshold, lengthTreshold):
         a[i]=a[i]/float(nbMots)
     return a
 
-text = word_tokenize("""The aim of a probabilistic logic (also probability logic and probabilistic reasoning) is to combine the capacity of probability theory to handle uncertainty with the capacity of deductive logic to exploit structure of formal argument. The result is a richer and more expressive formalism with a broad range of possible application areas. Probabilistic logics attempt to find a natural extension of traditional logic truth tables: the results they define are derived through probabilistic expressions instead. A difficulty with probabilistic logics is that they tend to multiply the computational complexities of their probabilistic and logical components. Other difficulties include the possibility of counter-intuitive results, such as those of Dempster-Shafer theory. The need to deal with a broad variety of contexts and issues has led to many different proposals.""")
 #if __name__ == "__main__":
+#    text = word_tokenize("""The aim of a probabilistic logic (also probability logic and probabilistic reasoning) is to combine the capacity of probability theory to handle uncertainty with the capacity of deductive logic to exploit structure of formal argument. The result is a richer and more expressive formalism with a broad range of possible application areas. Probabilistic logics attempt to find a natural extension of traditional logic truth tables: the results they define are derived through probabilistic expressions instead. A difficulty with probabilistic logics is that they tend to multiply the computational complexities of their probabilistic and logical components. Other difficulties include the possibility of counter-intuitive results, such as those of Dempster-Shafer theory. The need to deal with a broad variety of contexts and issues has led to many different proposals.""")
 #    w= textToDictionnary(t, [])
 #    d = frequency(w)
 #    e = frequencyWithTreshold(w, 2, 0)
@@ -158,4 +152,82 @@ def textToDictionnary(text, blackList):
         dic[word] = f[word]
     return dic
     
+"""Estimating the language of a text"""
 
+
+
+def estimate_language(text):
+    """returns the most probable language of the text.
+     @param text: Text you want the language
+     @type text: str
+     
+     @return: Most probable language guessed
+     @rtype: str
+     """
+    words = [word.lower() for word in nltk.wordpunct_tokenize(text)]
+    best_language = ''
+    score = 0
+    for language in nltk.corpus.stopwords.fileids():
+        stopw = nltk.corpus.stopwords.words(language)
+        common= set(words).intersection(set(stopw))
+#        print language,":",common
+        if len(common) > score:
+            best_language = language
+            score = len(common)
+           # print "New best"
+    return best_language
+
+if __name__ == "__main__":
+    t = """If the score is close to 0 (appearing in black),
+this means that the consensus between input clusterings is
+very high; it appears in white for unstable data, i.e. data that
+are classiﬁed with different neighbors according to the input
+clusterings. We observe, consistently with the preceding ob-
+servations on clusters, that most pixels are very stable. The
+unstable ones seem to be located at the frontier of objects, in
+the dark areas of the consensually classiﬁed image. Again,
+it would indicate to the interpreters that these pixels (corre-
+sponding to the previously mentioned black clusters) deserve"""
+    print estimate_language(t)
+    
+    t = """Albert Einstein (en allemand : [ˈalbɐt ˈaɪnʃtaɪn] Prononciation du titre dans sa version originale Écouter) né le 14 mars 1879 N 1 à Ulm, Wurtemberg, et mort le 18 avril 1955 à Princeton, New Jersey est un physicien théoricien qui fut successivement allemand, apatride (1896), suisse (1901) et sous la double nationalité helvético-américaine (1940)1. Il publie sa théorie de la relativité restreinte en 1905, et sa théorie de la gravitation dite relativité générale en 1915. Il contribue largement au développement de la mécanique quantique et de la cosmologie, et reçoit le prix Nobel de physique de 1921 pour son explication de l’effet photoélectrique2. Son travail est notamment connu du grand public pour l’équation E=mc2, qui établit une équivalence entre la matière et l’énergie d’un système.Il est aujourd'hui considéré comme l'un des plus grands scientifiques de l'histoire, et sa renommée dépasse largement le milieu scientifique. Il est la personnalité du XXe siècle selon l'hebdomadaire Time."""
+    print estimate_language(t)
+    
+    t = u"""Acknowledgements
+
+This work has been partially supported by the Conseil r´egional
+d’ˆIle-de-France under a doctoral allowance of its program R´eseau
+de Recherche Doctoral en Math´ematiques de l’ˆIle de France
+(RDM-IdF) for the period 2012 - 2015 and by the Labex LMH
+(ANR-11-IDEX-003-02).
+
+References
+
+[1] Pierre Alquier and Olivier Wintenberger. Model selection
+for weakly dependent time series forecasting. Bernoulli,
+18(3) : 883-913, 2012.
+
+[2] Olivier Catoni and Jean Picard. Statistical Learning The-
+ory and Stochastic Optimization: Ecole D’´et´e de Prob-
+abilit´es de Saint-Flour XXXI-2001. Number n 1851 in
+Ecole d’ ´Et´e de Probabilit´es de Saint-Flour. Springer-
+Verlag, 2004.
+
+[3] Krzysztof Łatuszy´nski and Wojciech Niemiro. Rigorous
+conﬁdence bounds for MCMC under a geometric drift
+condition. J. Complexity, 27(1) : 23-38, 2011.
+
+[4] K. L. Mengersen and R. L. Tweedie. Rates of convergence
+of the Hastings and Metropolis algorithms. Ann. Statist.,
+24(1) : 101-121, 1996.
+
+[5] Eric Moulines, Pierre Priouret, and Franc¸ois Roueﬀ. On
+recursive estimation for time varying autoregressive pro-
+cesses. Ann. Statist., 33(6) : 2610-2654, 2005.
+
+[6] Emmanuel Rio. In´egalit´es de Hoeﬀding pour les fonc-
+tions lipschitziennes de suites d´ependantes. Comptes
+Rendus de l’Acad´emie des Sciences Series I Mathemat-
+ics, 330(10):905908, 2000."""
+
+    print textToDictionnary(t, [])
